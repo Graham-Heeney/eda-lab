515a93a5d3868386409859f524f06f3d9ad5bf2d
Graham-Heeney <20102466@mail.wit.ie>
2025-11-25 14:45:39 +0000

    replaced download logic with db write statement

--- lambdas/processImage.ts
+++ lambdas/processImage.ts
@@ -1,5 +1,8 @@
 /* eslint-disable import/extensions, import/no-absolute-path */
 import { SQSHandler } from "aws-lambda";
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import { DynamoDBDocumentClient, PutCommand } from "@aws-sdk/lib-dynamodb";
+
 import {
   GetObjectCommand,
   PutObjectCommandInput,
@@ -15,6 +18,7 @@ export const handler: SQSHandler = async (event) => {
   for (const record of event.Records) {
     const recordBody = JSON.parse(record.body);        // Parse SQS message
     const snsMessage = JSON.parse(recordBody.Message); // Parse SNS message
+const ddbDocClient = createDDbDocClient();
 
     if (snsMessage.Records) {
       console.log("Record body ", JSON.stringify(snsMessage));
@@ -22,20 +26,39 @@ export const handler: SQSHandler = async (event) => {
         const s3e = s3Message.s3;
         const srcBucket = s3e.bucket.name;
         // Object key may have spaces or unicode non-ASCII characters.
-        const srcKey = decodeURIComponent(s3e.object.key.replace(/\+/g, " "));
-        let theImage = null;
-        try {
-          // Download the image from the S3 source bucket.
-          const params: GetObjectCommandInput = {
-            Bucket: srcBucket,
-            Key: srcKey,
-          };
-          theImage = await s3.send(new GetObjectCommand(params));
-          // Process the image ......
-        } catch (error) {
-          console.log(error);
-        }
+      const srcKey = decodeURIComponent(s3e.object.key.replace(/\+/g, " "));
+      let theImage = null;
+      try {
+        await ddbDocClient.send(
+          new PutCommand({
+            TableName: process.env.TABLE_NAME,
+            Item: {
+              name: srcKey,
+            },
+          })
+        );
+        theImage = await s3.send(new GetObjectCommand({ Bucket: srcBucket, Key: srcKey }));
+        // Process the image ......
+      } catch (error) {
+        console.log(error);
+      }
       }
     }
   }
+
+  
+function createDDbDocClient() {
+  const ddbClient = new DynamoDBClient({ region: process.env.REGION });
+  const marshallOptions = {
+    convertEmptyValues: true,
+    removeUndefinedValues: true,
+    convertClassInstanceToMap: true,
+ };
+  const unmarshallOptions = {
+    wrapNumbers: false,
+ };
+  const translateConfig = { marshallOptions, unmarshallOptions };
+  return DynamoDBDocumentClient.from(ddbClient, translateConfig);
+}
+
 };
--- lib/eda-app-stack.ts
+++ lib/eda-app-stack.ts
@@ -8,6 +8,7 @@ import * as sqs from "aws-cdk-lib/aws-sqs";
 import * as sns from "aws-cdk-lib/aws-sns";
 import * as subs from "aws-cdk-lib/aws-sns-subscriptions";
 import * as iam from "aws-cdk-lib/aws-iam";
+import * as dynamodb from "aws-cdk-lib/aws-dynamodb";
 
 import { Construct } from "constructs";
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
@@ -31,18 +32,31 @@ export class EDAAppStack extends cdk.Stack {
       displayName: "New Image topic",
     }); 
 
+    const imagesTable = new dynamodb.Table(this, "ImagesTable", {
+      billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
+      partitionKey: { name: "name", type: dynamodb.AttributeType.STRING },
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      tableName: "Imagess",
+ });
+
   // Lambda functions
 
-    const processImageFn = new lambdanode.NodejsFunction(
+       const processImageFn = new lambdanode.NodejsFunction(
       this,
-      "ProcessImage",
-      {
+      "ProcessImageFn",
+ {
         runtime: lambda.Runtime.NODEJS_18_X,
         entry: `${__dirname}/../lambdas/processImage.ts`,
         timeout: cdk.Duration.seconds(15),
         memorySize: 128,
-      }
-    );
+        environment: {
+          TABLE_NAME: imagesTable.tableName,
+          BUCKET_NAME: imagesBucket.bucketName,
+          REGION: 'eu-west-1'
+ },
+ }
+ );
+
 
         const mailerQ = new sqs.Queue(this, "mailer-q", {
       receiveMessageWaitTime: cdk.Duration.seconds(10),
@@ -55,7 +69,6 @@ export class EDAAppStack extends cdk.Stack {
       timeout: cdk.Duration.seconds(3),
       entry: `${__dirname}/../lambdas/mailer.ts`,
     });
-
 
     // S3 --> SQS
         imagesBucket.addEventNotification(
@@ -100,7 +113,8 @@ mailerFn.addEventSource(newImageMailEventSource);
     imagesBucket.grantRead(processImageFn);
 
     // Output
-    
+        imagesTable.grantReadWriteData(processImageFn);
+
     newImageTopic.addSubscription(new subs.SqsSubscription(mailerQ));
 
 
